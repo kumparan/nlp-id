d8638bb70bbfdd788d6cda1693741271bc34ce0f
Frandy Eddy <frandy.eddy@kumparan.com>
2019-12-10 16:48:18 +0700

    update __init__.py

--- nlp_id/__init__.py
+++ nlp_id/__init__.py
@@ -1,3 +1,5 @@
 from nlp_id.lemmatizer import Lemmatizer
+from nlp_id import tokenizer
+from nlp_id import postag
 from nlp_id.tokenizer import Tokenizer, PhraseTokenizer
 from nlp_id.postag import PosTag
