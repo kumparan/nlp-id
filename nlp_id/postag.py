f164d4a6df55c68c2daec444ce32ce38c26cdb87
sayid fadilah <sayid.fadilah@kumparan.com>
2019-12-11 12:50:39 +0700

    added code for self train model

--- nlp_id/postag.py
+++ nlp_id/postag.py
@@ -3,11 +3,16 @@ from nlp_id import tokenizer
 import pickle
 import os
 import nltk
+# default classifier
+from sklearn import ensemble
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import Pipeline
 
 class PosTag:
-    def __init__(self):
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-        model_path = os.path.join(current_dir, 'data', 'postagger.pkl')
+    def __init__(self, model_path=None):
+        self.current_dir = os.path.dirname(os.path.realpath(__file__))
+        if not model_path:
+            model_path = os.path.join(self.current_dir, 'data', 'postagger.pkl')
         self.clf = self.load_model(model_path)
         self.tokenizer = tokenizer.Tokenizer()
 
@@ -15,6 +20,11 @@ class PosTag:
         pickle_in = open(model_path,"rb")
         load_data = pickle.load(pickle_in)
         return load_data
+
+    def save_model(self,model_path):
+        pickle_out = open(model_path, "wb")
+        pickle.dump(self.clf, pickle_out)
+        pickle_out.close()
 
     def features(self, sentence, index):
         """ sentence: [w1, w2, ...], index: the index of the word """
@@ -88,3 +98,45 @@ class PosTag:
         tag = self.get_pos_tag(text)
         phrase_tag = self.chunk_tag(tag)
         return phrase_tag
+
+    def transform_to_dataset(self, sentences, tags):
+        X, y = [], []
+
+        for sentence_idx in range(len(sentences)):
+            for index in range(len(sentences[sentence_idx])):
+                X.append(self.features(sentences[sentence_idx], index))
+                y.append(tags[sentence_idx][index])
+
+        return X, y
+
+    def read_dataset(self, dataset_path=None):
+
+        if not dataset_path:
+            dataset_path = os.path.join(self.current_dir, 'data', 'dataset_postag.txt')
+
+        with open(dataset_path) as f:
+            raw_file = f.read().split("\n")
+
+        files = [i.split("\t") for i in raw_file]
+
+        sentences, tags, temp_sentences, temp_tags = [], [], [], []
+
+        for file in files:
+            if file != [""]:
+                temp_sentences.append(file[0]) # get the sentences
+                temp_tags.append(file[1]) # get the tag
+            else:
+                # check if the temp sentences and temp tags is not null and both of them have the same length
+                if len(temp_sentences) > 0 and len(temp_tags) > 0 and (len(temp_sentences) == len(temp_tags)):
+                    sentences.append(temp_sentences)
+                    tags.append(temp_tags)
+                temp_sentences, temp_tags = [], []
+        return sentences, tags
+
+    def train(self, sentences, tags):
+        self.clf = Pipeline([
+            ('vectorizer', DictVectorizer(sparse=True)),
+            ('classifier', ensemble.RandomForestClassifier(criterion='gini', n_estimators=15))
+        ])
+
+        self.clf.fit(sentences, tags)
