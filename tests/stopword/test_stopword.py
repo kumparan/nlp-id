dfa13fe50d86892f09f69bfebde474547af82175
Zavli Juwantara <zavli.juwantara7@gmail.com>
2020-07-15 16:59:31 +0700

    add stopword remover test

--- tests/stopword/test_stopword.py
+++ tests/stopword/test_stopword.py
@@ -9,3 +9,12 @@ def test_stopword():
     stopword = StopWord()
     stopword = stopword.get_stopword()
     assert len(stopword) == number_of_nlp_id_stopword
+
+
+def test_remove_stopword():
+    text = "Lionel Messi pergi Ke pasar di area Jakarta Pusat."
+    stopword = StopWord()
+    x = stopword.remove_stopword(text)
+    print(x)
+    expected_result = "Lionel Messi pergi pasar area Jakarta Pusat."
+    assert stopword.remove_stopword(text) == expected_result
